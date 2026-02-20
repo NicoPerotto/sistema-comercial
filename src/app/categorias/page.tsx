b7e8ef4569bfe9ba9733f5df331a699378a89514
Nico Perotto <nicolasperottoghi@gmail.com>
2026-02-20 18:16:17 -0300

    feat: Implement category management page and new sales creation page.

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -28,6 +28,20 @@ const AVAILABLE_ICONS = {
     Carrot: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.5 3.5c-1.5-1.5-4-1.5-5.5 0L6 12.5c-1 1-1 2.5 0 3.5l2 2c1 1 2.5 1 3.5 0l9-9c1.5-1.5 1.5-4 0-5.5z M8.5 17.5l-3 3 M11.5 14.5l-2 2" />,
     Milk: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 2h10l1 4v14a2 2 0 01-2 2H8a2 2 0 01-2-2V6l1-4z M7 8h10" />,
     Pizza: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11l-3 3 3 3 m-6-6l3 3-3 3 M12 3L3 20h18L12 3z" />,
+    Meat: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.5 14.5c1.333-1.333 4.5-4.5 4.5-4.5s2.5-1.5 1-3-3-1-3-1l-4.5 4.5-3-3L11 8l2.5 2.5L11 13l2.5 2.5L11 18l2.5 2.5 3.5-3.5-1.5-2.5z M10 13l-4 4a2 2 0 000 2.828l.172.172a2 2 0 002.828 0L13 16" />,
+    Fish: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M22 12s-4-4-10-4-10 4-10 4 4 4 10 4 10-4 10-4z M17 12a1 1 0 11-2 0 1 1 0 012 0z" />,
+    IceCream: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 2a4 4 0 00-4 4v2a4 4 0 108 0V6a4 4 0 00-4-4z M12 14l-4 8h8l-4-8z" />,
+    Cake: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12c0-4.97-4.03-9-9-9s-9 4.03-9 9m18 0v6c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2v-6m18 0H3m12-5l-3 3-3-3" />,
+    Beer: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 8h1a3 3 0 013 3v2a3 3 0 01-3 3h-1m-4 4H7a2 2 0 01-2-2V5a2 2 0 012-2h7a2 2 0 012 2v12a2 2 0 01-2 2z M7 8h9" />,
+    Wine: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 2h8m-4 0v12m0 0a4 4 0 01-4 4h8a4 4 0 01-4-4m-6 4h12" />,
+    Cleaning: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 11l5-5m0 0l5 5m-5-5v12m-8 4h16" />,
+    Pets: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />,
+    Tools: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z M12 15a3 3 0 100-6 3 3 0 000 6z" />,
+    Office: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.414a4 4 0 00-5.656-5.656l-6.415 6.414a6 6 0 108.486 8.486L19 13" />,
+    Tech: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />,
+    Shirt: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 7V3m0 4l-4-4m4 4l4-4M5 7L3 9v12h18V9l-2-2m-2 0h-2m-4 0H9m-2 0H5" />,
+    Gift: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v13m0-13c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm0 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zM5 8h14v13H5V8z" />,
+    Medicine: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />,
 };
 
 interface Category {
